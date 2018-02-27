0d02a6167d0f20f06cee0a558931383495e0e462
Elb4t <eloy4t@gmail.com>
2018-02-27 19:58:19 +0100

    Crear un test unitario con parámetros

--- app/src/test/java/es/elb4t/calculadoratesting/MathOperationTest.kt
+++ app/src/test/java/es/elb4t/calculadoratesting/MathOperationTest.kt
@@ -1,21 +1,35 @@
 package es.elb4t.calculadoratesting
 
+import junitparams.JUnitParamsRunner
+import junitparams.Parameters
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.*
+
 
 /**
  * Created by eloy on 27/2/18.
  */
+@RunWith(JUnitParamsRunner::class)
 class MathOperationTest {
+
+    @Parameters(method = "getValidAdditionInput")
     @Test
-    fun additionShouldReturnTenWhenOperandsAreThreeAndSeven() {
+    fun additionShouldReturnExpectedValueWhenOperandsAreValid(operand1: Double, operand2: Double, expectedValue: Double) {
         //Arrange
-        val operation: MathOperation = MathOperation()
-        val expectedValue: Double = 10.0
-        var result: Double = 0.0
+        val mathOperation = MathOperation()
         //Act
-        result = operation.addition(3.0, 7.0)
+        var result = mathOperation.addition(operand1, operand2)
         //Assert
-        assertEquals(String.format("Resultado %1s ha de ser igual a %2s",result, expectedValue), expectedValue, result, 0.0)
+        assertEquals(String.format("Resultado %1s ha de ser igual a %2s", result, expectedValue), expectedValue, result, 0.0)
+    }
+
+    private fun getValidAdditionInput(): Collection<Array<Any>> {
+        return Arrays.asList(
+                arrayOf<Any>(1, 1, 2),
+                arrayOf<Any>(2, -2, 0),
+                arrayOf<Any>(-3.5, -2.7, -6.2)
+        )
     }
 }
