a7d372caa1e621f9195e59ec316d21e217d4b795
Elb4t <eloy4t@gmail.com>
2018-02-28 22:28:11 +0100

    Crear una Suite

--- app/src/test/java/es/elb4t/calculadoratesting/MathOperationTest.kt
+++ app/src/test/java/es/elb4t/calculadoratesting/MathOperationTest.kt
@@ -4,6 +4,7 @@ import com.google.common.truth.Truth.assertThat
 import junitparams.JUnitParamsRunner
 import junitparams.JUnitParamsRunner.`$`
 import junitparams.Parameters
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -24,9 +25,40 @@ class MathOperationTest {
         mathOperation = MathOperation()
     }
 
+    @Test
+    fun additionShouldReturnTenWhenOperandsAreThreeAndSeven() {
+        //Arrange
+        val operation: MathOperation = MathOperation()
+        val expectedValue: Double = 10.0
+        var result: Double = 0.0
+        //Act
+        result = operation.addition(3.0, 7.0)
+        //Assert
+        assertEquals(String.format("Resultado %1s ha de ser igual a %2s",result, expectedValue), expectedValue, result, 0.0)
+    }
+
+    @Parameters(method = "getValidAdditionInput")
+    @Test
+    fun additionShouldReturnExpectedValueWhenOperandsAreValid(operand1: Double, operand2: Double, expectedValue: Double) {
+        //Arrange
+        val mathOperation = MathOperation()
+        //Act
+        var result = mathOperation.addition(operand1, operand2)
+        //Assert
+        assertEquals(String.format("Resultado %1s ha de ser igual a %2s", result, expectedValue), expectedValue, result, 0.0)
+    }
+
+    private fun getValidAdditionInput(): Collection<Array<Any>> {
+        return Arrays.asList(
+                arrayOf<Any>(1, 1, 2),
+                arrayOf<Any>(2, -2, 0),
+                arrayOf<Any>(-3.5, -2.7, -6.2)
+        )
+    }
+
     @Parameters(method = "getInvalidInput")
     @Test(expected = OperationException::class)
-    fun additionShouldReturnExpectedValueWhenOperandsAreValid(operand1: Double, operand2: Double) {
+    fun additionShouldThrowsWhenValuesAreInvalid(operand1: Double, operand2: Double) {
         //Act
         var result = mathOperation!!.addition(operand1, operand2)
     }
@@ -42,7 +74,7 @@ class MathOperationTest {
     }
 
     @Parameters(method = "getValidExponentiationInput")
-    @Test
+    @Test(expected = OperationException::class, timeout = 100)
     fun exponentiationShouldReturnExpectedValueWhenInputIsValid(
             base: Double, exponent: Double, expectedValue: Double) {
         val result = mathOperation!!.exponentiation(base, exponent)
---
+++ app/src/test/java/es/elb4t/calculadoratesting/MathSuite.kt
@@ -0,0 +1,13 @@
+package es.elb4t.calculadoratesting
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+
+/**
+ * Created by eloy on 28/2/18.
+ */
+@RunWith(Suite::class)
+@Suite.SuiteClasses(MathExpressionTest::class,
+                    MathOperationTest::class)
+class MathSuite
